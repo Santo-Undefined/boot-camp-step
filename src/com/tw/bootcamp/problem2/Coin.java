cfb54af81e9a90e4dbece5915d8c3ede7dad6252
Ajoy Bhunia <ajoybhunia@Ajoys-MacBook-Pro.local>
2026-05-07 14:45:06 +0530

    Removes constant field from coin

--- src/com/tw/bootcamp/problem2/Coin.java
+++ src/com/tw/bootcamp/problem2/Coin.java
@@ -2,14 +2,11 @@ package com.tw.bootcamp.problem2;
 
 public class Coin {
 
-    public static final double PROBABILITY_OF_GETTING_TAILS = 0.5;
-    public static final int MAX_PROBABILITY = 1;
-
     public double probabilityForTails() {
-        return PROBABILITY_OF_GETTING_TAILS;
+        return 0.5;
     }
 
     public double probabilityForHeads() {
-        return MAX_PROBABILITY - probabilityForTails();
+        return 1 - probabilityForTails();
     }
 }
