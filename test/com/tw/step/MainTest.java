57ef807c62395e75d3af08a96711748ed8a24455
Ajoy Bhunia <ajoybhunia@Ajoys-MacBook-Pro.local>
2026-05-07 11:01:31 +0530

    Initial commit

---
+++ test/com/tw/step/MainTest.java
@@ -0,0 +1,13 @@
+package com.tw.step;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertEquals(2,2);
+    }
+}
