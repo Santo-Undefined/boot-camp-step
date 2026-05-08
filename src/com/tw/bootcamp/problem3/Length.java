c67aa27b4f1952d2210abd3d0ed4615a5436c121
Santo K M <santokm@Santos-MacBook-Pro.local>
2026-05-08 11:27:55 +0530

    overrides equals in length

--- src/com/tw/bootcamp/problem3/Length.java
+++ src/com/tw/bootcamp/problem3/Length.java
@@ -9,7 +9,7 @@ public class Length {
 
     private static Length create(double value) throws NegativeLengthException {
         if (value < 0) throw new NegativeLengthException("Length can't be negative");
-        return  new Length(value);
+        return new Length(value);
     }
 
     public static Length feet(double value) throws NegativeLengthException {
@@ -24,7 +24,9 @@ public class Length {
         return create(value);
     }
 
-    public boolean isEqual(Length len) {
-        return value == len.value;
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Length length)) return false;
+        return Double.compare(value, length.value) == 0;
     }
 }
--- test/com/tw/bootcamp/problem3/LengthTest.java
+++ test/com/tw/bootcamp/problem3/LengthTest.java
@@ -9,23 +9,23 @@ class LengthTest {
     void shouldCompare1FeetWith12Inches() throws NegativeLengthException {
         Length feet = Length.feet(1);
         Length inch = Length.inch(12);
-        assertTrue(feet.isEqual(inch));
-        assertTrue(inch.isEqual(feet));
+        assertEquals(feet,inch);
+        assertEquals(inch,feet);
     }
 
     @Test
     void shouldCompareCmWithInches() throws NegativeLengthException {
         Length inch = Length.inch(2);
         Length cm = Length.cm(5);
-        assertTrue(inch.isEqual(cm));
-        assertTrue(cm.isEqual(inch));
+        assertEquals(inch,cm);
+        assertEquals(cm,inch);
     }
 
     @Test
     void shouldReturnFalseOnDifferentLength() throws NegativeLengthException {
         Length oneInch = Length.inch(1);
         Length oneCm = Length.cm(1);
-        assertFalse(oneInch.isEqual(oneCm));
+        assertNotEquals(oneInch,oneCm);
     }
 
     @Test
