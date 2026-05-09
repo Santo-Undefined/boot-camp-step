8acfa57a07ec3bb486b205d45ec307a6a93b0a76
Santo K M <santokm@Santos-MacBook-Pro.local>
2026-05-09 12:18:31 +0530

    half way in the assistant

--- src/com/tw/bootcamp/problem4/ParkingDeck.java
+++ src/com/tw/bootcamp/problem4/ParkingDeck.java
@@ -3,7 +3,7 @@ package com.tw.bootcamp.problem4;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ParkingDeck {
+public class ParkingDeck implements ParkingState {
     private final Map<Integer, ParkingLot> deck = new HashMap<>();
     private int id = 1;
 
@@ -15,5 +15,15 @@ public class ParkingDeck {
     public ParkingLot park(int parkingLotId) {
         ParkingLot parkingLot = deck.get(parkingLotId);
         return parkingLot.park();
+    }
+
+    @Override
+    public boolean isFull(int id) {
+        return deck.get(id).isFull();
+    }
+
+    @Override
+    public int remainingSlots(int id) {
+        return deck.get(id).remainingSlots();
     }
 }
